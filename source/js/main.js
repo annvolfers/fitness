f2e317c9adc4e6058ccdd29ceb801704f494a413
annvolfers <annvolfers@me.com>
2023-03-31 17:27:40 +0300

    Добавляет блок о тренажерном зале

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,7 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
+import {Video} from './modules/video/video';
 
 // ---------------------------------
 
@@ -17,6 +18,9 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
+    const video = new Video();
+    video.init();
+
     initModals();
     const form = new Form();
     window.form = form;
---
+++ source/js/modules/video/video.js
@@ -0,0 +1,56 @@
+export class Video {
+  constructor() {
+    this.container = document.querySelector('[data-container]');
+    this.video = document.querySelector('[data-video]');
+    this.playButton = document.querySelector('[data-play-button]');
+    this.youtubePlayer = null;
+
+    this.playButton.classList.add('disabled');
+    this.playButton.tabIndex = -1;
+  }
+
+  init() {
+    this.loadScript('https://www.youtube.com/player_api', this.initPlayer.bind(this));
+  }
+
+  playButtonClickHandler(evt) {
+    if (this.youtubePlayer.playVideo) {
+      evt.preventDefault();
+      this.container.classList.add('is-active');
+      this.youtubePlayer.playVideo();
+    }
+  }
+
+  initPlayer() {
+    window.onYouTubePlayerAPIReady = () => {
+      this.youtubePlayer = new YT.Player(this.video.id, {
+        videoId: this.video.id,
+        events: {
+          'onReady': this.enablePlayButton.bind(this),
+          'onStateChange': this.returnPlayButton.bind(this),
+        },
+      });
+    };
+  }
+
+  returnPlayButton(event) {
+    if (event.data === 0) {
+      this.container.classList.remove('is-active');
+    }
+  }
+
+  enablePlayButton() {
+    this.playButton.classList.remove('disabled');
+    this.playButton.tabIndex = 0;
+
+    this.playButton.addEventListener('click', this.playButtonClickHandler.bind(this));
+  }
+
+  loadScript(src, handleLoad) {
+    const scriptElement = document.createElement('script');
+    scriptElement.src = src;
+    document.body.append(scriptElement);
+
+    scriptElement.addEventListener('load', handleLoad);
+  }
+}
